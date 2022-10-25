27a92bf864f589e1b75015ab400e0cf1665925c4
João Vitor Queiroz <joaovitor.queiroz@outlook.com.br>
2022-10-25 00:47:16 -0300

    feat: ensure AccountMongoRepository returns an account on success

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,12 +1,18 @@
-import { MongoClient } from 'mongodb'
+import { Collection, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
   client: null as unknown as MongoClient,
+
   async connect (url: string): Promise<void> {
-    this.client = new MongoClient(global.__MONGO_URI__)
+    this.client = new MongoClient(url)
     await this.client.connect()
   },
+
   async disconnect (): Promise<void> {
     await this.client.close()
+  },
+
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
   }
 }
