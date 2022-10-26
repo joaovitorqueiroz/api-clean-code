8831ae3cc17aebc8cb6a05905c28d2e569147b70
João Vitor Queiroz <joaovitor.queiroz@outlook.com.br>
2022-10-26 01:57:22 -0300

    feat: ensure requests parse body as json

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+/* istanbul ignore file */
+import app from './config/app'
 
-const app = express()
 app.listen(5050, () => console.log('server running at http://localhost:5050'))
